10dd006331ae96d7f1c49404aa87c43713d3baac
Dipayan Bose <bosedipayan2000@gmail.com>
2025-04-19 21:39:59 +0530

    Modification of sidebar

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -1,7 +1,17 @@
+import { Search } from "lucide-react";
 import React from "react";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="flex gap-2 items-center p-2 border rounded-md max-w-xl">
+      <Search />
+      <input
+        type="text"
+        placeholder="Search Here..."
+        className="outline-none"
+      />
+    </div>
+  );
 }
 
 export default Header;
--- app/dashboard/_components/SideNav.tsx
+++ app/dashboard/_components/SideNav.tsx
@@ -1,7 +1,51 @@
+import Image from "next/image";
 import React from "react";
+import logo from "../../../public/logo.svg";
+import { FileClock, Home, Settings, WalletCards } from "lucide-react";
 
 function SideNav() {
-  return <div>SideNav</div>;
+  const MenuList = [
+    {
+      name: "Home",
+      icon: Home,
+      path: "/dashboard",
+    },
+    {
+      name: "History",
+      icon: FileClock,
+      path: "/dashboard/history",
+    },
+    {
+      name: "Billing",
+      icon: WalletCards,
+      path: "/dashboard/billing",
+    },
+    {
+      name: "Setting",
+      icon: Settings,
+      path: "/dashboard/setting",
+    },
+  ];
+
+  return (
+    <div className="h-screen p-5 shadow-sm">
+      <div className="flex justify-center">
+        <Image src={logo} alt="Logo" width={75} height={55} />
+      </div>
+
+      <div className="mt-5">
+        {MenuList.map((menu, index) => (
+          <div
+            className="flex gap-2 mb-2 p-3 
+          hover:bg-orange-400 hover:text-white rounded-lg cursor-pointer"
+          >
+            <menu.icon />
+            <h2>{menu.name}</h2>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default SideNav;
